1a20d9b798769a7cdfae6fa39000443aae6f0aa6
KEK-W8 <73126556+KEK-W8@users.noreply.github.com>
2020-11-16 21:47:49 +0300

    Updated 16/11/2020

---
+++ 01_cash_counter/js/script.js
@@ -0,0 +1,8 @@
+let totalCash = Number(prompt('Skolko seichas deneg est? (dollarov)'));
+let watchesCount = Number(prompt('Skolko watches hochetsa kupit? (shtook)'));
+let earringsCount = Number(prompt('Skolko serejek hochetsa kupit? (shtook)'));
+
+let watchesSum = Number(prompt('Skolko stoyat odin watches? (dollarov)') * parseInt(watchesCount));
+let earringsSum = Number(prompt('Skolko stoyat odin serejka? (dollarov)') * parseInt(earringsCount));
+
+document.write(totalCash >= watchesSum + earringsSum);
